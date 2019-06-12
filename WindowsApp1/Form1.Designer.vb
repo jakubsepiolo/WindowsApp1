a9d3bfe15f8c4f6775cc0ad4f126c4b5fbd30e07
Jakub SEPIOLO <18JS3026@barton.ac.uk>
2019-06-12 14:15:16 +0100

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(12, 16)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Size = New System.Drawing.Size(1204, 594)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(953, 616)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1228, 642)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Chart1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Button1 As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,81 @@
+﻿
+Imports System.Windows.Forms.DataVisualization
+
+Public Class Form1
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Chart1_Click(sender As Object, e As EventArgs) 
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        Chart1.Titles.Add("Line of x^2") ' specify chart name
+        Chart1.ChartAreas.Clear()
+        Dim ChartArea1 As New Charting.ChartArea
+        ChartArea1 = Chart1.ChartAreas.Add("Default")
+
+        With ChartArea1
+            With .AxisX
+                .Crossing = 0
+                .Minimum = -720
+                .Maximum = 720
+                .MajorGrid.Enabled = True
+                .LineWidth = 2
+                .Interval = 180
+                .MajorGrid.LineDashStyle = Charting.ChartDashStyle.Dot
+            End With
+            With .AxisY
+                .Crossing = 0
+                .Minimum = -5
+                .Maximum = 5
+                .LineWidth = 2
+                .Interval = 1
+                .MajorGrid.LineDashStyle = Charting.ChartDashStyle.Dot
+            End With
+        End With
+
+        With Chart1.ChartAreas("Default")
+
+            .AxisX.Title = "X" ' x label
+            .AxisX.MajorGrid.LineColor = Color.SkyBlue
+            .AxisY.MajorGrid.LineColor = Color.SkyBlue
+            .AxisY.Title = "Y" 'y label
+        End With
+
+        'specify series plot lines 
+        Chart1.Series.Clear()
+        Chart1.Series.Add("plot1")
+        Chart1.Series("plot1").Color = Color.Red
+        Chart1.Series("plot1").ChartType = DataVisualization.Charting.SeriesChartType.Line
+
+
+        Dim n As Integer = 720 ' number of points
+
+        Dim y As Single
+        For x As Integer = -n To n Step 1
+            y = Math.Cos((Math.PI * x) / 180)
+            Chart1.Series("plot1").Points.AddXY(x, y)
+        Next
+
+        Chart1.Series.Add("plot2")
+        Chart1.Series("plot2").Color = Color.Blue
+        Chart1.Series("plot2").ChartType = DataVisualization.Charting.SeriesChartType.Line
+        For x As Integer = 1 To n Step 1
+            y = Math.Sin((Math.PI * x) / 180)
+            Chart1.Series("plot2").Points.AddXY(x, y)
+        Next
+
+        Chart1.Series.Add("plot3")
+        Chart1.Series("plot3").Color = Color.Gray
+        Chart1.Series("plot3").ChartType = DataVisualization.Charting.SeriesChartType.Line
+        For x As Integer = 1 To n Step 1
+            y = Math.Sqrt(x) / 25
+            Chart1.Series("plot3").Points.AddXY(x, y)
+        Next
+
+    End Sub
+End Class
