12e3e5f2c6410c743a7cf25a6c2a7959936c86f4
18js3026 <18js3026@L2243>
2019-06-18 15:57:12 +0100

    cccc

---
+++ aaaaaaaaaa/aaaaaaaaaa/ComplexNumber.vb
@@ -0,0 +1,49 @@
+﻿Public Class ComplexNumber
+    Public Real As Single
+    Public Complex As Single
+    Public Sub New(ByVal RealPart As Single, ByVal ComplexPart As Single)
+        Real = RealPart
+        Complex = ComplexPart
+    End Sub
+
+    Public Shared Operator +(ByVal ComplexNumber1 As ComplexNumber, ByVal ComplexNumber2 As ComplexNumber) As ComplexNumber
+        Return New ComplexNumber(ComplexNumber1.Real + ComplexNumber2.Real, ComplexNumber1.Complex + ComplexNumber2.Complex)
+    End Operator
+    Public Shared Operator -(ByVal ComplexNumber1 As ComplexNumber, ByVal ComplexNumber2 As ComplexNumber) As ComplexNumber
+        Return New ComplexNumber(ComplexNumber1.Real - ComplexNumber2.Real, ComplexNumber1.Complex - ComplexNumber2.Complex)
+    End Operator
+    Public Shared Operator *(ByVal ComplexNumber1 As ComplexNumber, ByVal ComplexNumber2 As ComplexNumber) As ComplexNumber
+        Return New ComplexNumber(ComplexNumber1.Real * ComplexNumber2.Real - ComplexNumber1.Complex * ComplexNumber2.Complex, ComplexNumber1.Real * ComplexNumber2.Complex + ComplexNumber2.Real * ComplexNumber1.Complex)
+    End Operator
+    Public Shared Operator /(ByVal ComplexNumber1 As ComplexNumber, ByVal ComplexNumber2 As ComplexNumber) As ComplexNumber
+        Return New ComplexNumber((ComplexNumber1.Real * ComplexNumber2.Real + ComplexNumber1.Complex * ComplexNumber2.Complex) / (ComplexNumber2.Real * ComplexNumber2.Real + ComplexNumber2.Complex * ComplexNumber2.Complex), (ComplexNumber1.Complex * ComplexNumber2.Real - ComplexNumber1.Real * ComplexNumber2.Complex) / (ComplexNumber2.Real * ComplexNumber2.Real + ComplexNumber2.Complex * ComplexNumber2.Complex))
+    End Operator
+    Public Shared Operator =(ByVal ComplexNumber1 As ComplexNumber, ByVal ComplexNumber2 As ComplexNumber) As Boolean
+        Return ComplexNumber1.Real = ComplexNumber2.Real And ComplexNumber1.Complex = ComplexNumber2.Complex
+    End Operator
+
+    Public Shared Operator <>(ByVal ComplexNumber1 As ComplexNumber, ByVal ComplexNumber2 As ComplexNumber) As Boolean
+        Return ComplexNumber1.Real <> ComplexNumber2.Real And ComplexNumber1.Complex <> ComplexNumber2.Complex
+    End Operator
+    Public Shared Operator >(ByVal ComplexNumber1 As ComplexNumber, ByVal ComplexNumber2 As ComplexNumber) As Boolean
+        Return ComplexNumber1.Modulus > ComplexNumber2.Modulus
+    End Operator
+    Public Shared Operator <(ByVal ComplexNumber1 As ComplexNumber, ByVal ComplexNumber2 As ComplexNumber) As Boolean
+        Return ComplexNumber1.Modulus < ComplexNumber2.Modulus
+    End Operator
+    Public Function Modulus() As Single
+        Return Math.Sqrt(Me.Real ^ 2 + Me.Complex ^ 2)
+    End Function
+    Public Function Argument() As Single
+        Return Math.Atan(Me.Complex / Me.Real)
+    End Function
+    Public Function RealPart() As Single
+        Return Real
+    End Function
+    Public Function ComplexPart() As Single
+        Return Complex
+    End Function
+    Public Function ModulusArgument() As String
+        Return $"{Modulus()}, θ{Argument()}"
+    End Function
+End Class
---
+++ aaaaaaaaaa/aaaaaaaaaa/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MyNumber As New ComplexNumber(3, 5)
+        Dim TheirNumber As New ComplexNumber(-2, -4)
+        Dim Totalnumber As New ComplexNumber(0, 0)
+        Totalnumber = MyNumber - TheirNumber
+        Console.WriteLine(Totalnumber.Real)
+        Console.WriteLine(Totalnumber.Complex)
+        Console.WriteLine(Totalnumber.ModulusArgument)
+        Console.ReadKey()
+    End Sub
+
+End Module
